0a89d12537248580fa0eec981de91fedb81353f9
Te3Ka <Mnement4813@yandex.ru>
2024-10-04 09:24:08 +0300

    Add ProgressiveBar -- Добавил бар прогрессии, для наглядного отображения получения данных IP -- Выставил разрешение только на портретную ориентацию приложения

--- app/src/main/java/ru/te3ka/getmyipaddress/MainActivity.kt
+++ app/src/main/java/ru/te3ka/getmyipaddress/MainActivity.kt
@@ -1,13 +1,16 @@
 package ru.te3ka.getmyipaddress
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import ru.te3ka.getmyipaddress.databinding.ActivityMainBinding
 import ru.te3ka.getmyipaddress.service.RetrofitInstance
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,12 +28,19 @@ class MainActivity : AppCompatActivity() {
             myIpViewModel.lastIp.collectLatest { ip ->
                 binding.textViewMyIpAddress.text = ip?.myIp ?: getString(
                     R.string.no_ip_address)
+                binding.progressBarCircleLoadMyIp.visibility = View.GONE
             }
         }
 
         binding.buttonRefresh.setOnClickListener {
+            binding.progressBarCircleLoadMyIp.visibility = View.VISIBLE
             binding.textViewMyIpAddress.text = getString(R.string.refreshing)
-            myIpViewModel.fetchAndSaveIp(RetrofitInstance.api)
+
+            lifecycleScope.launch {
+                val randomDeleay = Random.nextLong(200, 5000)
+                delay(randomDeleay)
+                myIpViewModel.fetchAndSaveIp(RetrofitInstance.api)
+            }
         }
     }
 }
